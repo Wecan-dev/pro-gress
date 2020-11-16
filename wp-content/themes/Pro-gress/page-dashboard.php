3a49f342a27626a422574927c4fc84f3565b3d43
dannyunico <danielbermudezreyes@gmail.com>
2020-11-16 10:04:19 -0400

    add new page

--- wp-content/themes/Pro-gress/page-dashboard.php
+++ wp-content/themes/Pro-gress/page-dashboard.php
@@ -1,23 +1,5 @@
 <?php login(''); get_header(); ?>
 <section>
-  <div class="navabar__grub">
-
-
-        <ul class="sidebar__list--menu pb-4 mt-5">
-          <li><a href="#!" class="active"><img src="<?php echo get_template_directory_uri();?>/assets/img/icon-home.png" alt="">Dashboard</a></li>
-          <li><a href="#!"><img src="<?php echo get_template_directory_uri();?>/assets/img/icon-reports.png" alt="">Reportes</a></li>
-          <li><a href="<?php bloginfo('url'); ?>/workout"><img src="<?php echo get_template_directory_uri();?>/assets/img/icon-dumbell.png" alt="">Workouts</a></li>
-          <li><a href="#!"><img src="<?php echo get_template_directory_uri();?>/assets/img/icon-calendar.png" alt="">Calendario</a></li>
-        </ul>
-        <ul class="sidebar__list--menu py-4">
-          <li><a href="<?php bloginfo('url'); ?>/pro-videos"><img src="<?php echo get_template_directory_uri();?>/assets/img/icon-video.png" alt="">Pro-videos</a></li>
-          <li><a href="#!"><img src="<?php echo get_template_directory_uri();?>/assets/img/icon-library.png" alt="">Exercises List</a></li>
-        </ul>
-      </div>
-      <div class="btn-soporte">
-        <a href="#">Soporte <i class="fa fa-angle-down" aria-hidden="true"></i></a>
-      </div>
-    </div>
     <?php get_template_part('partials/menu/sidebar__navbar'); ?>
 
     <div class="sidebar__content">
